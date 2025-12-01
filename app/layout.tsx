20fd6be312a5ae5487e06b19daf459118554af6e
mrfaizanshariff <mrfaizanshariff@gmail.com>
2025-12-01 14:04:32 +0530

    added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from '@/components/ThemeProvider';
 import Navbar from '@/components/layout/Navbar';
 import Footer from '@/components/layout/Footer';
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { LanguageProvider } from '@/context/LanguageContext';
 import localFont from 'next/font/local';
 
@@ -139,6 +140,7 @@ export default function RootLayout({
             <Navbar />
             {children}
             <Analytics/>
+            <SpeedInsights/>
             <Footer />
           </LanguageProvider>
         </ThemeProvider>
